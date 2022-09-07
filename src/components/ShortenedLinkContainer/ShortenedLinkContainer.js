cc3cfce7aa73bd0b59600deb585605f3cbdcb6b3
DenisStoimenov <96194514+DenisStoimenov@users.noreply.github.com>
2022-09-07 14:58:15 +0200

    add feature copy button for shortened links

--- src/components/ShortenedLinkContainer/ShortenedLinkContainer.js
+++ src/components/ShortenedLinkContainer/ShortenedLinkContainer.js
@@ -1,19 +1,29 @@
+import copy from 'copy-to-clipboard';
+import { useState } from 'react';
+
+
 function ShortenedLinkContainer(props) {
 
+const [linkToCopy, setlinkToCopy] = useState(props.value.short_link)
+const [linkState, setlinkState] = useState('Copy')
+function handleCopy(){
+    copy(linkToCopy)
+    setlinkState('Copied!')
+}
     return (
         <div className="link-wrapper">
             <div>
                 <p className="full-link">{props.value.original_link}</p>
                 <p className="short-link" id="short-link">
-                    {props.value.short_link}
+                    {linkToCopy}
                 </p>
             </div>
             <button
-                className="button"
-                id="copy-link-button"
+                className={linkState === 'Copy' ? 'button' : 'button active'}
+                onClick = {handleCopy}
 
             >
-                Copy
+                {linkState}
             </button>
         </div>
     );
